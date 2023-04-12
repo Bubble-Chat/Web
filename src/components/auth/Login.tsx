37dfcf4eb2d38d3262a8f95dba86985daacb2adf
bae0203 <gmbae06@dgsw.hs.kr>
2023-04-12 18:27:44 +0900

    ADD :: Icon

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div>
+        <nav>
+          <div>Icon</div>
+          <p>Slogan1</p>
+          <p>Slogan2</p>
+        </nav>
+      </div>
+      <div></div>
+    </>
+  );
 };
