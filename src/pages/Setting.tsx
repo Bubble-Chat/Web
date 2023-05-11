fbdcfdece18f704ec34c7782ba747a9e45826b17
bae0203 <gmbae06@dgsw.hs.kr>
2023-05-11 10:42:27 +0900

    ADD :: Setting Contents

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import SideBar from "../components/common/sidebar/SideBar";
+import * as S from "../styles/pages/Setting.style";
 
 const Setting = () => {
   return (
     <>
-      <SideBar title="설정" index={4} />
+      <SideBar title="설정" index={4}>
+        <>
+          <S.ContentsContainer>
+            <S.Icon></S.Icon>
+            <S.ContentsContext>버블챗 공지</S.ContentsContext>
+          </S.ContentsContainer>
+          <S.ContentsContainer>
+            <S.Icon></S.Icon>
+            <S.ContentsContext>버블챗 사용법</S.ContentsContext>
+          </S.ContentsContainer>
+          <S.ContentsContainer>
+            <S.Icon></S.Icon>
+            <S.ContentsContext>고객 센터</S.ContentsContext>
+          </S.ContentsContainer>
+          <S.ContentsContainer>
+            <S.Icon></S.Icon>
+            <S.ContentsContext>차단 맴버 관리</S.ContentsContext>
+          </S.ContentsContainer>
+        </>
+      </SideBar>
       <div>Setting</div>
     </>
   );
---
+++ src/styles/pages/Setting.style.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Icon = styled.div`
+  background-color: gray;
+  width: 1.5rem;
+  height: 1.5rem;
+  margin-right: 1rem;
+`;
+
+export const ContentsContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  width: 92%;
+  height: 1.5rem;
+  padding: 4%;
+`;
+
+export const ContentsContext = styled.p`
+  font-size: 1.125rem;
+  font-weight: 400;
+  letter-spacing: 0.2rem;
+  color: #515151;
+`;
