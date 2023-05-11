fb18f7cbc45aafe85a533328761562d17b79a705
bae0203 <gmbae06@dgsw.hs.kr>
2023-05-11 10:42:38 +0900

    똥싼거

--- src/pages/Storage.tsx
+++ src/pages/Storage.tsx
@@ -4,7 +4,11 @@ import SideBar from "../components/common/sidebar/SideBar";
 const Storage = () => {
   return (
     <>
-      <SideBar title="저장소" index={3} />
+      <SideBar title="저장소" index={3}>
+        <>
+          <div>dd</div>
+        </>
+      </SideBar>
       <div>Storage</div>
     </>
   );
