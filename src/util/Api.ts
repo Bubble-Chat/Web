cf5c4122d32aaf1cdebe480d89cfbb302076d98b
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-04 20:10:43 +0900

    fix : baseUrl

--- src/util/Api.ts
+++ src/util/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://34aa-221-168-22-204.ngrok-free.app",
+  baseURL: "https://36ad-221-168-22-204.ngrok-free.app",
 });
 
 export default API;
