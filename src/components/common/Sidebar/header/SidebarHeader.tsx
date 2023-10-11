b9fefdccb99ebc14d52f0cb5323ca112b394bc4c
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-11 20:53:15 +0900

    fix : Components

--- src/components/common/Sidebar/header/SidebarHeader.tsx
+++ src/components/common/Sidebar/header/SidebarHeader.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import * as S from "./SidebarHeader.style";
+import Icon from "../../Icon";
+import AddFriend from "../../../../asset/Icon/AddFriends.svg";
+import AddChatRoom from "../../../../asset/Icon/AddChatRoom.svg";
+import Search from "../../../../asset/Icon/Search.svg";
 
 interface IHeaderProps {
   title: string;
@@ -11,9 +15,9 @@ const SidebarHeader = (props: IHeaderProps) => {
     <S.MainHeaderContainer>
       <S.TitleElement>{props.title}</S.TitleElement>
       <S.IconContainer>
-        {props.index === 1 && <S.IconElement />}
-        {props.index !== 3 && props.index !== 4 && <S.IconElement />}
-        {props.index !== 4 && <S.IconElement />}
+        {props.index === 1 && <Icon url={AddFriend} />}
+        {props.index !== 3 && props.index !== 4 && <Icon url={AddChatRoom} />}
+        {props.index !== 4 && <Icon url={Search} />}
       </S.IconContainer>
     </S.MainHeaderContainer>
   );
