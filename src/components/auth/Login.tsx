8e65eede21464f614658091c70961ce1ebe59f75
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-11 18:11:52 +0900

    fix : login Design

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,51 +1,11 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import * as S from "../../styles/Login.style";
-import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
 import API from "../../util/Api";
 import { toast } from "react-toastify";
 export const Login = () => {
   const Navigate = useNavigate();
-  // const googleSocialLogin = useGoogleLogin({
-  //   scope: "email profile",
-  //   onSuccess: async (res) => {
-  //     console.log("dfdfd", res);
 
-  //     API.post("/api/v1/user/signin", null, {
-  //       params: {
-  //         provider: "google",
-  //         token: code,
-  //       },
-  //     })
-  //       .then((e) => console.log("d", e))
-  //       .catch((e) => console.log("ddd", e));
-
-  // let xhr = new XMLHttpRequest();
-
-  // let params1: any = {
-  //   provider: "google",
-  //   token: code,
-  // };
-
-  // xhr.addEventListener("readystatechange", () => {
-  //   if (xhr.readyState == 4) {
-  //     console.log(xhr.status, xhr.response);
-  //   }
-  // });
-
-  // xhr.open(
-  //   "get",
-  //   "http://10.80.161.132.nip.io:8080/api/v1/user/signin?provider=google&token=" +
-  //     code
-  // );
-  // xhr.responseType = "json";
-  // xhr.send();
-  //   },
-  //   onError: (errorResponse) => {
-  //     console.error(errorResponse);
-  //   },
-  //   flow: "auth-code",
-  // });
   return (
     <S.MainNav>
       <S.ContentContainer>
@@ -65,12 +25,7 @@ export const Login = () => {
                 .catch((e) => console.log("ddd", e));
             }}
           />
-          {/* <S.LoginBtn onClick={googleSocialLogin}>카카오로 시작하기</S.LoginBtn>
-          <S.LoginBtn onClick={googleSocialLogin}>네이버로 시작하기</S.LoginBtn> */}
         </S.LoginBtnWrap>
-        <S.LoginStateBtn onClick={() => Navigate("/home/")}>
-          계정이 있다면 로그인하기
-        </S.LoginStateBtn>
       </S.ContentContainer>
     </S.MainNav>
   );
--- src/styles/Login.style.ts
+++ src/styles/Login.style.ts
@@ -23,33 +23,3 @@ export const LoginBtnWrap = styled.div`
   display: flex;
   flex-direction: column;
 `;
-
-export const LoginBtn = styled.p`
-  padding: 1rem 1.8rem;
-  border-radius: 0.3rem;
-  font-size: 1.125rem;
-  color: white;
-  background-color: #9867ff;
-  display: inline-block;
-  font-weight: 600;
-  margin: 0rem auto 1rem auto;
-  &:hover {
-    background-color: #7d4ee0;
-  }
-  &:active {
-    background-color: #56369a;
-  }
-`;
-
-export const LoginStateBtn = styled.p`
-  margin-top: 1rem;
-  color: #9867ff;
-  font-weight: 600;
-  font-size: 0.8rem;
-  &:hover {
-    color: #7d4ee0;
-  }
-  &:active {
-    color: #56369a;
-  }
-`;
