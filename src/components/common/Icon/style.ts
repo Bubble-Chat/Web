6e09b6182f7f3e34505ac78232d60a25ff357232
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-11 20:51:46 +0900

    fix : color

--- src/components/common/Icon/style.ts
+++ src/components/common/Icon/style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 interface IImageStyleProps {
-  isActive: boolean;
+  isActive?: boolean;
 }
 
 export const Image = styled.img`
@@ -10,10 +10,10 @@ export const Image = styled.img`
 
   filter: ${({ isActive }: IImageStyleProps) =>
     isActive
-      ? `invert(90%) sepia(19%) saturate(3673%) hue-rotate(193deg)
-  brightness(104%) contrast(103%)`
-      : `invert(70%) sepia(60%) saturate(4412%) hue-rotate(225deg)
-      brightness(100%) contrast(103%)`};
+      ? `invert(70%) sepia(60%) saturate(4412%) hue-rotate(225deg)
+      brightness(100%) contrast(103%)`
+      : `invert(90%) sepia(19%) saturate(3673%) hue-rotate(193deg)
+      brightness(104%) contrast(103%)`};
   &:hover {
     filter: invert(86%) sepia(26%) saturate(7237%) hue-rotate(210deg)
       brightness(100%) contrast(102%);
