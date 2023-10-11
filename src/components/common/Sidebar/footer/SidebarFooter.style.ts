7dbf38a64e628b6d7b47c3392d8521144e8605e2
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-11 20:51:39 +0900

    fix : change name

---
+++ src/components/common/Sidebar/footer/SidebarFooter.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const MainFooterContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%;
+  margin: 5% 10%;
+  position: absolute;
+  bottom: 0;
+  background-color: white;
+`;
