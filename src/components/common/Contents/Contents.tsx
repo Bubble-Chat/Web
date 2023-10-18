650dc64f60fd47533c172babe3aa3a0b577825fb
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-18 20:11:48 +0900

    fix : Navigate

--- src/components/common/Contents/Contents.tsx
+++ src/components/common/Contents/Contents.tsx
@@ -1,28 +1,39 @@
 import React from "react";
 import * as S from "./style";
 import { IContentsStyleProps } from "./style";
+import { useNavigate } from "react-router-dom";
 
 interface IContentsProps extends IContentsStyleProps {
+  location: string;
   title: string;
+  subtitle?: string;
   context: string;
-  image: string | null;
+  image?: string | null;
+  contentIdx: number;
 }
 
 const Contents = ({
+  location,
   title,
+  subtitle,
   context,
   image,
   imageBorder,
   titleFontSize,
   contextFontSize,
+  contentIdx,
 }: IContentsProps) => {
+  const Navigate = useNavigate();
   return (
-    <S.MainContiner>
+    <S.MainContiner onClick={() => Navigate(`/${location}/${contentIdx}`)}>
       <S.InfoImage imageBorder={imageBorder}>
         {image && <img src={image} alt="df" />}
       </S.InfoImage>
       <S.InfoContextContents>
-        <S.InfoTitle titleFontSize={titleFontSize}>{title}</S.InfoTitle>
+        <S.TitleContainer>
+          <S.InfoTitle titleFontSize={titleFontSize}>{title}</S.InfoTitle>
+          <S.InfoSubTitle>{subtitle}</S.InfoSubTitle>
+        </S.TitleContainer>
         <S.InfoContextContext contextFontSize={contextFontSize}>
           {context}
         </S.InfoContextContext>
