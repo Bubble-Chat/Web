38df277ce55ebc18b487386184b8ff55a92368b5
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-18 20:10:15 +0900

    fix : Page Template

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "./Auth";
-import Freind from "./Friend";
-import Room from "./Room";
-import Setting from "./Setting";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import { Colors } from "../styles/theme";
+import FriendPage from "./FriendPage";
+import RoomPage from "./RoomPage";
+import SettingPage from "./SettingPage";
 
 const Router = () => {
   return (
@@ -18,11 +18,11 @@ const Router = () => {
           <ToastContainer />
           <Routes>
             <Route path="/" element={<Auth />} />
-            <Route path={"/home"} element={<Freind />} />
-            <Route path={"/home/:id"} element={<Freind />} />
-            <Route path="/rooms" element={<Room />} />
-            <Route path="/rooms/:id" element={<Room />} />
-            <Route path="/setting" element={<Setting />} />
+            <Route path="/home" element={<FriendPage />} />
+            <Route path="/home/:id" element={<FriendPage />} />
+            <Route path="/rooms" element={<RoomPage />} />
+            <Route path="/rooms/:id" element={<RoomPage />} />
+            <Route path="/setting" element={<SettingPage />} />
           </Routes>
         </BrowserRouter>
       </ThemeProvider>
---
+++ src/pages/SettingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Setting from "../components/Setting";
+
+const SettingPage = () => {
+  return (
+    <>
+      <Setting />
+    </>
+  );
+};
+
+export default SettingPage;
