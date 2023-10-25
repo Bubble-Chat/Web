570a5515318f8707da6964c55e623bc2748cdc54
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-25 18:13:15 +0900

    add : axiois header

--- src/util/Api.ts
+++ src/util/Api.ts
@@ -3,6 +3,9 @@ import serverUrl from "../config/config.json";
 
 const API = axios.create({
   baseURL: serverUrl.serverUrl,
+  headers: {
+    Authorization: localStorage.getItem("accessToken"),
+  },
 });
 
 export default API;
