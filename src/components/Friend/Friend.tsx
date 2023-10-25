7483538dba8f9b8afcd3d740e4b8d987118cf5ba
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-25 18:14:54 +0900

    add : modal, add dummy data

--- src/components/Friend/Friend.tsx
+++ src/components/Friend/Friend.tsx
@@ -3,6 +3,10 @@ import { useParams } from "react-router-dom";
 import SideBar from "../common/Sidebar/SideBar";
 import Contents from "../common/Contents";
 import Info from "./Info";
+import API from "../../util/Api";
+import Search from "./Search";
+import { useRecoilState } from "recoil";
+import { isActiveSearchAtom } from "../../store/friendAtom";
 
 interface Ivalue {
   name: string;
@@ -13,12 +17,32 @@ interface Ivalue {
 
 const Friend = () => {
   const { id } = useParams();
+  const [isActiveSearch, setIsActiveSearch] =
+    useRecoilState<boolean>(isActiveSearchAtom);
   const value: Ivalue[] = [
     {
-      name: "나",
-      introduce: "안녕하세요 웹싸개 입니다.",
-      id: "gmbae06@gmail.com",
-      idx: 0,
+      name: "조근호",
+      introduce: "안녕하세요 iOS싸개 입니다.",
+      id: "whqkrqkr1@icloud.com",
+      idx: 1,
+    },
+    {
+      name: "우영기",
+      introduce: "안녕하세요 안드싸개 입니다.",
+      id: "wkpark@gmail.com",
+      idx: 2,
+    },
+    {
+      name: "이재진",
+      introduce: "안녕하세요 서버싸개 입니다.",
+      id: "jaejin@gmail.com",
+      idx: 3,
+    },
+    {
+      name: "김민균",
+      introduce: "안녕하세요 안드싸개 입니다.",
+      id: "kimmin@gamil.com",
+      idx: 4,
     },
     {
       name: "조근호",
@@ -47,6 +71,7 @@ const Friend = () => {
   ];
   return (
     <div style={{ display: "flex" }}>
+      {isActiveSearch && <Search setIsActiveSearch={setIsActiveSearch} />}
       <SideBar title="친구창" index={1}>
         <>
           {value.map((val, index) => {
@@ -65,6 +90,16 @@ const Friend = () => {
           })}
         </>
       </SideBar>
+      <button
+        onClick={() => {
+          API.patch("/api/v1/user/change", { name: "조근호" })
+            .then((e) => console.log(e))
+            .catch((e) => console.log(e));
+        }}
+      >
+        dfdfs
+      </button>
+      <button>dfd</button>
       {id && <Info />}
     </div>
   );
