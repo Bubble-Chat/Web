3741183ee635bacd645d014d4041dc1de3d60842
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-25 18:14:26 +0900

    add : logout

--- src/components/Setting/Setting.tsx
+++ src/components/Setting/Setting.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import SideBar from "../common/Sidebar/SideBar";
 import * as S from "./style";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Setting = () => {
+  const Navigate = useNavigate();
   return (
     <>
       <SideBar title="설정" index={4}>
@@ -22,6 +25,16 @@ const Setting = () => {
           <S.ContentsContainer>
             <S.Icon></S.Icon>
             <S.ContentsContext>차단 맴버 관리</S.ContentsContext>
+          </S.ContentsContainer>
+          <S.ContentsContainer
+            onClick={() => {
+              localStorage.removeItem("accessToken");
+              toast.error("로그아웃하셨습니다.");
+              Navigate("/");
+            }}
+          >
+            <S.Icon></S.Icon>
+            <S.ContentsContext>로그아웃</S.ContentsContext>
           </S.ContentsContainer>
         </>
       </SideBar>
