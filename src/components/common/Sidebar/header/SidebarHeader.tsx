2abee109f6390208535234df67c12da532c78988
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-25 18:15:21 +0900

    add : search icon recoil set state

--- src/components/common/Sidebar/header/SidebarHeader.tsx
+++ src/components/common/Sidebar/header/SidebarHeader.tsx
@@ -4,20 +4,27 @@ import Icon from "../../Icon";
 import AddFriend from "../../../../asset/Icon/AddFriends.svg";
 import AddChatRoom from "../../../../asset/Icon/AddChatRoom.svg";
 import Search from "../../../../asset/Icon/Search.svg";
+import { useSetRecoilState } from "recoil";
+import { isActiveSearchAtom } from "../../../../store/friendAtom";
 
 interface IHeaderProps {
   title: string;
   index: number;
 }
 
-const SidebarHeader = (props: IHeaderProps) => {
+const SidebarHeader = ({ title, index }: IHeaderProps) => {
+  const setIsActiveSearch = useSetRecoilState(isActiveSearchAtom);
   return (
     <S.MainHeaderContainer>
-      <S.TitleElement>{props.title}</S.TitleElement>
+      <S.TitleElement>{title}</S.TitleElement>
       <S.IconContainer>
-        {props.index === 1 && <Icon url={AddFriend} />}
-        {props.index !== 3 && props.index !== 4 && <Icon url={AddChatRoom} />}
-        {props.index !== 4 && <Icon url={Search} />}
+        {index === 1 && <Icon url={AddFriend} />}
+        {index !== 3 && index !== 4 && <Icon url={AddChatRoom} />}
+        {index !== 4 && (
+          <div onClick={() => setIsActiveSearch(true)}>
+            <Icon url={Search} />
+          </div>
+        )}
       </S.IconContainer>
     </S.MainHeaderContainer>
   );
