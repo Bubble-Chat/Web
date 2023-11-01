77c46761250ebdb9030ecac5a5a6ff3b75aa66f2
bae0203 <gmbae06@dgsw.hs.kr>
2023-11-01 20:35:18 +0900

    Add : Modal state

--- src/store/friendAtom.ts
+++ src/store/friendAtom.ts
@@ -4,3 +4,13 @@ export const isActiveSearchAtom = atom<boolean>({
   key: "isActiveSearchAtom",
   default: false,
 });
+
+export const isActiveAddFriendAtom = atom<boolean>({
+  key: "isActiveAddFriendAtom",
+  default: false,
+});
+
+export const isActiveAddRoomAtom = atom<boolean>({
+  key: "isActiveAddRoomAtom",
+  default: false,
+});
