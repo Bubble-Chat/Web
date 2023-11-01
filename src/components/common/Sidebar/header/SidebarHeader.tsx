165e560f06a08ba40312f8b4340a4ee291bad00d
bae0203 <gmbae06@dgsw.hs.kr>
2023-11-01 20:36:18 +0900

    fix : Add Sidebar Header OnClick Handler

--- src/components/common/Sidebar/header/SidebarHeader.tsx
+++ src/components/common/Sidebar/header/SidebarHeader.tsx
@@ -5,7 +5,11 @@ import AddFriend from "../../../../asset/Icon/AddFriends.svg";
 import AddChatRoom from "../../../../asset/Icon/AddChatRoom.svg";
 import Search from "../../../../asset/Icon/Search.svg";
 import { useSetRecoilState } from "recoil";
-import { isActiveSearchAtom } from "../../../../store/friendAtom";
+import {
+  isActiveAddFriendAtom,
+  isActiveAddRoomAtom,
+  isActiveSearchAtom,
+} from "../../../../store/friendAtom";
 
 interface IHeaderProps {
   title: string;
@@ -14,12 +18,22 @@ interface IHeaderProps {
 
 const SidebarHeader = ({ title, index }: IHeaderProps) => {
   const setIsActiveSearch = useSetRecoilState(isActiveSearchAtom);
+  const setIsActiveAddRoom = useSetRecoilState(isActiveAddRoomAtom);
+  const setIsActiveAddFriend = useSetRecoilState(isActiveAddFriendAtom);
   return (
     <S.MainHeaderContainer>
       <S.TitleElement>{title}</S.TitleElement>
       <S.IconContainer>
-        {index === 1 && <Icon url={AddFriend} />}
-        {index !== 3 && index !== 4 && <Icon url={AddChatRoom} />}
+        {index === 1 && (
+          <div onClick={() => setIsActiveAddFriend(true)}>
+            <Icon url={AddFriend} />
+          </div>
+        )}
+        {index !== 3 && index !== 4 && (
+          <div onClick={() => setIsActiveAddRoom(true)}>
+            <Icon url={AddChatRoom} />
+          </div>
+        )}
         {index !== 4 && (
           <div onClick={() => setIsActiveSearch(true)}>
             <Icon url={Search} />
