df5ebd4516b35f1ddb78cc0fdd33aaaf0eef02aa
bae0203 <gmbae06@dgsw.hs.kr>
2023-11-01 20:35:25 +0900

    Add : Modal

--- src/components/Template/DefaultTemplate.tsx
+++ src/components/Template/DefaultTemplate.tsx
@@ -1,13 +1,33 @@
 import React, { ReactElement } from "react";
 import { Background } from "./style";
+import { useRecoilState } from "recoil";
+import {
+  isActiveAddFriendAtom,
+  isActiveAddRoomAtom,
+  isActiveSearchAtom,
+} from "../../store/friendAtom";
+import Search from "./modals/Search";
+import AddFriend from "./modals/AddFriend";
+import AddRoom from "./modals/AddRoom";
 
 interface IDefaultTemplateProps {
   children: ReactElement;
 }
 
 const DefaultTemplate = ({ children }: IDefaultTemplateProps) => {
+  const [isActiveSearch, setIsActiveSearch] =
+    useRecoilState<boolean>(isActiveSearchAtom);
+  const [isActiveAddRoom, setIsActiveAddRoom] =
+    useRecoilState<boolean>(isActiveAddRoomAtom);
+  const [isActiveAddFriend, setIsActiveAddFriend] = useRecoilState<boolean>(
+    isActiveAddFriendAtom
+  );
+
   return (
     <>
+      {isActiveSearch && <Search setActive={setIsActiveSearch} />}
+      {isActiveAddFriend && <AddFriend setActive={setIsActiveAddFriend} />}
+      {isActiveAddRoom && <AddRoom setActive={setIsActiveAddRoom} />}
       <Background />
       {children}
     </>
