3ab939dd06e96f8fff09a4be8d32d2cb86773bc5
bae0203 <gmbae06@dgsw.hs.kr>
2023-11-07 11:24:12 +0900

    Add : Search Friend

--- src/components/Template/modals/Search/Search.tsx
+++ src/components/Template/modals/Search/Search.tsx
@@ -1,6 +1,10 @@
 import React, { Dispatch, SetStateAction, useState } from "react";
 import { relateFriend, searchFriend } from "../../../../asset/Apis/Friend";
-import * as S from "./style";
+import * as S from "../style";
+import Modal from "../../../common/Modal";
+import Input from "../../../common/Input";
+import Button from "../../../common/Button";
+import { ISearchFriendsValue } from "./type";
 
 const Search = ({
   setActive,
@@ -8,13 +12,15 @@ const Search = ({
   setActive: Dispatch<SetStateAction<boolean>>;
 }) => {
   const [name, setName] = useState<string>("");
-  const [Email, setEmail] = useState<string>("");
+
+  const [newFriendList, setNewFriendList] = useState<ISearchFriendsValue[]>([]);
 
   const searchFriendHandler = () => {
-    if (name !== "") searchFriend(name);
-  };
-  const relateFriendHandler = () => {
-    if (Email !== "") relateFriend(Email);
+    if (name !== "")
+      searchFriend(name).then((value) => {
+        console.log(value);
+        setNewFriendList(value.userList);
+      });
   };
 
   const setActiveModal = () => {
@@ -22,32 +28,40 @@ const Search = ({
   };
 
   return (
-    <S.Container>
-      <S.MainContentsBox>
-        <div onClick={setActiveModal} className="pointer">
-          X
-        </div>
-        <div>친구 찾기</div>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button onClick={searchFriendHandler} className="pointer">
-          찾기
-        </button>
-        <div>친구 추가</div>
-        <input
-          type="text"
-          value={Email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button onClick={relateFriendHandler} className="pointer">
-          추가
-        </button>
-      </S.MainContentsBox>
-      <S.Container onClick={setActiveModal}></S.Container>
-    </S.Container>
+    <>
+      <Modal title="친구 찾기" handler={setActiveModal}>
+        <S.InputContentWrap>
+          <S.InputWrap>
+            <S.InputTitle>친구 찾기</S.InputTitle>
+            <Input
+              state={name}
+              setState={setName}
+              placeholder="친구의 이름을 입력해주세요."
+              padding="16px"
+              width={250}
+              fontSize={14}
+            />
+            <Button
+              handler={searchFriendHandler}
+              fontSize="12px"
+              padding="10px 14px"
+              margin="0px 0px 0px 20px"
+              height="50px"
+            >
+              찾기
+            </Button>
+          </S.InputWrap>
+          <S.FriendList>
+            {newFriendList.map((val, idx) => (
+              <div key={idx}>
+                <p>{val.name}</p>
+                <p>{val.email}</p>
+              </div>
+            ))}
+          </S.FriendList>
+        </S.InputContentWrap>
+      </Modal>
+    </>
   );
 };
 
